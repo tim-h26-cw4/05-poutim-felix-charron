e59569ae8a9bf790258b678533665e8afabeb904
SpacePheonix <felixcha.2005@gmail.com>
2026-02-26 14:26:42 -0500

    Etape 1 fini en ajoutant la boucle dans la class main

---
+++ src/scripts/Chef.js
@@ -0,0 +1,8 @@
+export default class Chef {
+  constructor(element) {
+    console.log('Chef the movie from Cold Ones!');
+    this.element = element;
+    this.init();
+  }
+  init() {}
+}
--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,17 @@
-import Icons from "./utils/Icons.js";
+import Icons from './utils/Icons.js';
+import Chef from './Chef.js';
 
-// La ligne suivante devrait être au TOUT début du init() du Main
-Icons.load();
+class Main {
+  constructor() {
+    this.init();
+  }
+  init() {
+    Icons.load();
+    const chefs = document.querySelectorAll('[data-component="Chef"]');
+    for (let i = 0; i < chefs.length; i++) {
+      const chef = chefs[i];
+      new Chef(chef);
+    }
+  }
+}
+new Main();
